bec1ceddd01b0af464630dd4b83080d28d77784e
Abdulrahman Alnashri <d7.fx99@gmail.com>
2024-10-31 20:53:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
+import numpy as np
+import pickle
 
-st.title('🎈 App Name')
+# loading the saved model
+model = pickle.load(open('trained_model.sav', 'rb'))
 
-st.write('Hello world!')
+
+##streamlit app
+st.title('Iris Prediction')
+
+sepal_length = st.number_input('Sepal Length')
+sepal_width = st.number_input('Sepal Width')
+petal_length = st.number_input('Petal Length')
+petal_width = st.number_input('Petal Width')
+
+
+input_data = [sepal_length, sepal_width, petal_length, petal_width]
+
+
+# code for Prediction
+result = ''
+
+# creating a button for Prediction
+
+if st.button('Result'):
+  input_data = np.asarray(input_data).reshape(1,-1)
+  prediction = model.predict(input_data)
+  result = prediction[0]
+
+
+st.success(result)
